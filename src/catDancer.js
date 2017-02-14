e66a8b01230f47a22181dd4de3a4f51d4a357003
abianco3 <anthony.bianco3@gmail.com>
2017-02-14 12:20:58 -0800

    Make all cats explode on hover

--- src/catDancer.js
+++ src/catDancer.js
@@ -1,6 +1,7 @@
 var CatDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.append('<img class="cat" src="src/cat1.png"></img>');
+  this.explode();
   this.width = $('body').width();
   this.height = $('body').height();
   this.targetY = this.width * Math.random();
@@ -10,7 +11,7 @@ var CatDancer = function(top, left, timeBetweenSteps) {
   this.left = left;
   this.slope = (this.targetX - this.top) / (this.targetY - this.left);
   
-};
+}; 
 
 CatDancer.prototype = Object.create(Dancer.prototype);
 
@@ -20,6 +21,7 @@ CatDancer.prototype.oldStep = Dancer.prototype.step;
 
 CatDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
+  
   
   if (!this.pause) {
     this.oldStep();
@@ -31,11 +33,35 @@ CatDancer.prototype.step = function() {
 
     newLeft = newLeft < 0 ? 0 : newLeft;
     newTop = newTop < 0 ? 0 : newTop;
-    newLeft = newLeft > this.width ? this.width : newLeft;
-    newTop = newTop > this.heigth ? this.heigth : newTop;
+    var rightBound = window.innerWidth - 75;  // cat size
+    var bottomBound = window.innerHeight - 75;  // cat size
+    newLeft = newLeft > rightBound ? rightBound : newLeft;
+    newTop = newTop > bottomBound ? bottomBound : newTop;
 
     this.setPosition(newTop, newLeft);
     this.top = newTop;
     this.left = newLeft;
   }
+ 
+};
+
+CatDancer.prototype.explode = function() {
+  var catObject = this;
+  var $catDisplay = this.$node;
+
+  var explodeImage = function explodeImage() {
+    $catDisplay.find('img').attr('src', 'src/explosion.png');
+    setTimeout(function() {
+      $catDisplay.remove();
+      // remove the object from dancers array
+      for (var i = 0; i < window.dancers.length; i++) {
+        if (window.dancers[i] === catObject) {
+          window.dancers.splice(i, 1);
+        }
+      }
+      //delete catObject;
+    }, 1000);
+  };
+
+  $catDisplay.find('img').hover(explodeImage);
 };
